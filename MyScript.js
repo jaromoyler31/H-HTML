8939fe3ca48d3608ae2c59acea09958dc0f12013
Jarom Oyler <bubba@Bubba.local>
2022-08-31 10:18:58 -0600

    pop up box

--- MyScript.js
+++ MyScript.js
@@ -1,29 +1,9 @@
-function openNav()
+function openmodal(){
+    document.getElementById('Modal').style.display = "flex";
 
-    document.getElementById("sidenav").style.left ='0px'
-    document.getElementById("openbtn").style.visibility ="hidden";
+}
 
+function closemodal() {
+    document.getElementById('Modal').style.display = "none";
 
-function closeNav()
-
-    document.getElementById("sidenav").style.left =' -200px';
-    document.getElementById("openbtn").style.visibility ="visible";
-
-
-window.onresize = ( ) =>{
-    if (window.innerWidth >700)
-        openNav();
-    else
-        closeNav();
-}   
-
-
-
-function openDaddy()
-
-    document.getElementById("pu").style.visibility ="visible";
-
-
-function closeDaddy()
-
-    document.getElementById("pu").style.visibility ="hidden";
+}
