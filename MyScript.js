bd2ef6e70722ba81e72ae424b2edeb7a9d50b233
Jarom Oyler <bubba@Bubba.local>
2022-08-31 09:48:54 -0600

    hello

--- MyScript.js
+++ MyScript.js
@@ -1,6 +1,6 @@
 function openNav()
 
-    document.getElementById("sidenav").style.left =' 0px'
+    document.getElementById("sidenav").style.left ='0px'
     document.getElementById("openbtn").style.visibility ="hidden";
 
 
@@ -16,3 +16,14 @@ window.onresize = ( ) =>{
     else
         closeNav();
 }   
+
+
+
+function openDaddy()
+
+    document.getElementById("pu").style.visibility ="visible";
+
+
+function closeDaddy()
+
+    document.getElementById("pu").style.visibility ="hidden";
